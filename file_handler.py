ddbda83571da361d6cf2353f676bb30ae18e6c39
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-01-03 06:08:58 +0100

    Created endpoints to upload images for a business and for products

---
+++ file_handler.py
@@ -0,0 +1,55 @@
+import os
+import secrets
+import shutil
+
+from datetime import datetime
+
+from fastapi import HTTPException, status, UploadFile
+from PIL import Image
+
+FILEPATH = "./static/images/"
+
+
+def get_stamp():
+    date_now = str(datetime.now())
+    cleaned = date_now.replace(" ", "").replace("-", "").replace(":", "")
+
+    return cleaned[::-1]
+
+
+async def save_image(file: UploadFile):
+    filename: str = file.filename
+    last_position = filename.rfind(".")
+    extension = filename[last_position:]
+
+    if extension not in (".png", ".jpg", ".jpeg"):
+        raise HTTPException(
+            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            detail="File uploaded should be of type png, jpg or jpeg"
+        )
+
+    token_hex = secrets.token_hex(16)
+    db_filename = token_hex + get_stamp() + extension
+    file_path = FILEPATH + db_filename
+
+    # save image to destination path
+    try:
+        with open(file_path, "wb") as destination:
+            shutil.copyfileobj(file.file, destination)
+    finally:
+        await file.close()
+
+    # resize image
+    with Image.open(file_path) as img:
+        img.resize(size=(200, 200))
+        img.save(file_path)
+
+    return db_filename
+
+
+def delete_image(filename: str):
+    if filename not in ("default.jpg", "defaultproduct.jpg"):
+        try:
+            os.remove(FILEPATH + filename)
+        except Exception:
+            print(f"Failed to delete {filename}.")
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
+from os import stat
 from typing import List, Optional, Type
 
-from fastapi import FastAPI, Request, HTTPException, status, Depends
+from fastapi import FastAPI, Request, HTTPException, status, Depends, File, UploadFile
+from fastapi.staticfiles import StaticFiles
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from tortoise import BaseDBAsyncClient
 from tortoise.contrib.fastapi import register_tortoise
+from tortoise.exceptions import DoesNotExist
 from tortoise.signals import post_save
 
 from authentication import hash_password, verify_token, generate_token, decode_token
 from emailsender import send_email
-from models import Business, Business_Pydantic, User, UserIn_Pydantic, UserOut_Pydantic
+from models import Business, Business_Pydantic, Product, User, UserIn_Pydantic, UserOut_Pydantic
+from file_handler import save_image, delete_image
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 templates = Jinja2Templates(directory="templates")
 oauth2_schema = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -94,6 +100,51 @@ async def user_login(user: User = Depends(get_current_user)):
             "business": business_.dict()
         }
     }
+
+
+@app.post("/image/business")
+async def upload_business_image(
+    file: UploadFile = File(...), user: User = Depends(get_current_user)
+):
+    db_filename = await save_image(file)
+
+    business = await Business.get(owner=user.id)
+    delete_image(business.logo)
+    business.logo = db_filename
+    await business.save()
+
+    return {"status": "ok", "message": "Successfully uploaded image."}
+
+
+@app.post("/image/product/{id}")
+async def upload_product_image(
+    id: int, file: UploadFile = File(...), user: User = Depends(get_current_user)
+):
+    db_filename = await save_image(file)
+
+    try:
+        product = await Product.get(id=id)
+    except DoesNotExist:
+        delete_image(db_filename)
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Product does not exist"
+        )
+
+    business = await Business.get(id=product.business)
+
+    if user.id != business.owner:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not authenticated to perform this action.",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+    delete_image(product.image)
+    product.image = db_filename
+    await product.save()
+
+    return {"status": "ok", "message": "Successfully uploaded image."}
 
 register_tortoise(
     app,
