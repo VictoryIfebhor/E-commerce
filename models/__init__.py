4146af4d656b836f047c90633a8223cc821bb383
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-01-20 12:59:18 +0100

    Moved the models into a package

---
+++ models/__init__.py
@@ -0,0 +1,25 @@
+from .models import (
+    Business,
+    Business_Pydantic,
+    BusinessIn_Pydantic,
+    Product,
+    Product_Pydantic,
+    ProductIn_Pydantic,
+    User,
+    User_Pydantic,
+    UserIn_Pydantic,
+    UserOut_Pydantic
+)
+
+__all__ = [
+    "Business",
+    "Business_Pydantic",
+    "BusinessIn_Pydantic",
+    "Product",
+    "Product_Pydantic",
+    "ProductIn_Pydantic",
+    "User",
+    "User_Pydantic",
+    "UserIn_Pydantic",
+    "UserOut_Pydantic"
+]
